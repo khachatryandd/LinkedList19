62f31ba2cb78d3bb2908343f8e2a6e26b7103c42
khachatryandd <khachatryandd19@gmail.com>
2024-03-17 23:39:19 +0400

    aaaaa

--- MyPostfix/MyPostfix.cs
+++ MyPostfix/MyPostfix.cs
@@ -10,38 +10,75 @@ namespace MyPostfix;
 internal class MyPostfix
 {
     public string[] array;
-    private string _operation;
+    public bool x;
     public int index = 0;
     MyStack<string> myStack = new MyStack.MyStack<string>();
-    public MyPostfix (string operation)
+   public void Run()
     {
-        _operation = operation;
+        string text = Input();
+        bool isValid = Validate(text);
+        array= new string[text.Length];    
+        if(isValid)
+        {
+            ReturnString(text);
+            Algorithms();
+            foreach(var i in array)
+            Console.WriteLine(i);
+        }
+        else { Console.WriteLine("sax vata"); }
+    }
+    public string Input()
+    {
+        string text = Console.ReadLine();
+        return text;
     }
     public string[] ReturnString(string operation)
     {
-       
-      for(int i =0; i< array.Length; i++)
+        for (int i = 0; i < operation.Length; i++)
         {
-            if (_operation[i] == ' ')
+            if (operation[i] == ' ')
             {
                 index++;
                 continue;
             }
             else
             {
-                array[index] += operation[i];
+                array[index] = operation[i].ToString();
+                
             }
-
         }
-      return array;
+        //array = operation.Split(' ');
+        return array;
     }
-    public int Algorithms()
+    public void Algorithms()
     {
         for (int i = 0; i <= index; i++)
         {
-            if (array[i] != "*" || array[i] != "/" || array[i] != "+"|| array[i] != "-");
-            
+            if (array[i] != "*" && array[i] != "/" && array[i] != "+" && array[i] != "-" && array[i] != " ")
+            {
+                myStack.Push(array[i]);
+            }
+            else
+            {
+                break;
+            }
         }
-        return 0;
+        
+    }
+    
+    public bool Validate(string text)
+    {
+        foreach (var t in text)
+        {
+            if (char.IsDigit(t) || t == '+' || t == '-' || t == '*' || t == '/' || t == ' ')
+            {
+                x = true;
+            }
+            else
+            {
+                x = false;
+            }
+        }
+        return x;
     }
 }
--- MyPostfix/Program.cs
+++ MyPostfix/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-        
+            MyPostfix myPostfix = new MyPostfix();
+
+            myPostfix.Run();
         }
     }
 }
